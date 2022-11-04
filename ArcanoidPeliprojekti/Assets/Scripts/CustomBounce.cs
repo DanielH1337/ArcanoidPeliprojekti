dfa95b6fe03f436c8f34919f58a16c4ec7cf4c61
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2022-11-04 14:58:19 +0200

    Scriptien kirjoitus palikat ja kimpoaminen

---
+++ ArcanoidPeliprojekti/Assets/Scripts/CustomBounce.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBounce : MonoBehaviour
+{
+
+    BoxCollider2D bc2D;
+
+    private void Awake()
+    {
+        bc2D = GetComponent<BoxCollider2D>();
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Find the relative point. (Ball.x - Paddle.x) / Paddle.widht
+        if (collision.gameObject.tag == "Ball")
+        {
+            float relativePosition = (collision.transform.position.x - transform.position.x) / bc2D.bounds.size.x;
+
+
+            //Change the velocity of the ball depending on the relative point.
+            collision.rigidbody.velocity = new Vector2(relativePosition,1).normalized * collision.rigidbody.velocity.magnitude;
+        }
+
+
+
+
+    }
+
+}
---
+++ ArcanoidPeliprojekti/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 30f;
+    [SerializeField] Vector2 launchdir = new Vector2(1, 4);
+    [SerializeField] GameObject ballPrefab;
+
+    Rigidbody2D rb2D;
+    Vector3 balloffset;
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        ball Ball = GetComponentInChildren<ball>();
+        balloffset = Ball.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, 0);
+
+        if(transform.childCount > 0 && Input.GetButtonDown("Jump"))
+        {
+            ball ball = GetComponentInChildren<ball>();
+            ball.Launch(launchdir);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ResetBall();
+        }
+    }
+
+    public void ResetBall()
+    {
+        ball Ball = Instantiate(ballPrefab).GetComponent<ball>();
+        Ball.transform.parent = transform;
+        Ball.transform.position = transform.position + balloffset;
+    }
+}
