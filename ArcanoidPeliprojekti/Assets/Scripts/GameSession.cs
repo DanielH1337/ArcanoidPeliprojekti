177f533425304c970392e7378eba098a42cbcc41
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2022-11-14 13:15:20 +0200

    scenet, gameoverteksti jne

--- ArcanoidPeliprojekti/Assets/Scripts/GameSession.cs
+++ ArcanoidPeliprojekti/Assets/Scripts/GameSession.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
@@ -13,9 +14,9 @@ public class GameSession : MonoBehaviour
     [SerializeField] TMP_Text highScoreText;
     [SerializeField] int startLives;
 
-    [SerializeField] Image blackScreen;
-    [SerializeField] TMP_Text gameOverText;
-    [SerializeField] Button restartButton;
+    [SerializeField] GameObject blackScreen;
+    [SerializeField] GameObject gameOverText;
+    [SerializeField] GameObject restartButton;
 
     int currentLives;
     int currentScore;
@@ -38,6 +39,9 @@ public class GameSession : MonoBehaviour
     void Start()
     {
         currentLives = startLives;
+
+        restartButton.GetComponent<Button>().onClick.AddListener(Reset);
+        currentHighScore = PlayerPrefs.GetInt("highscore");
         
     }
 
@@ -45,17 +49,29 @@ public class GameSession : MonoBehaviour
     void Update()
     {
         var Balls = GameObject.FindGameObjectsWithTag("Ball");
-        if (Balls.Length <= 0)
+        if (Balls.Length == 0)
         {
             DecreaseLives();
             GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().ResetBall();
 
         
         }
+
+        var blocks = GameObject.FindGameObjectsWithTag("Block");
+
+
+        if (blocks.Length == 0)
+        {
+            //Do winning stuff
+
+            win();
+
+        }
         
         
         livesText.text = currentLives.ToString();
         scoreText.text = currentScore.ToString();
+        highScoreText.text = currentHighScore.ToString();
 
 
     }
@@ -72,8 +88,53 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    private void win()
+    {
+        var numberOfScenes = SceneManager.sceneCountInBuildSettings;
+        var currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if(currentBuildIndex == numberOfScenes -1)
+        {
+            //Declare winner
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            //Load Next Scene
+            SceneManager.LoadScene(currentBuildIndex + 1);
+        }
+    }
+
     private void GameOver()
     {
-        throw new NotImplementedException();
+        blackScreen.SetActive(true);
+        gameOverText.SetActive(true);
+        restartButton.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    
+    private void Reset()
+    {
+        ResetScore();
+        ResetScreen();
+        ResetLevel1();
+    }
+    private void ResetScreen()
+    {
+        blackScreen.SetActive(false);
+        gameOverText.SetActive(false);
+        restartButton.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    private void ResetScore()
+    {
+        currentLives = startLives;
+        currentScore = 0;
+    }
+    private void ResetLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
